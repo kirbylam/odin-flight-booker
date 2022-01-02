b6d7f2bab71ff543c49e934cfdd753e5268ca23a
kirby_lam <kirbylam16@gmail.com>
2022-01-01 20:33:13 -0800

    Created Flight and Airport models. Created index that lets users search for flights.

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,17 @@
+class FlightsController < ApplicationController
+  def index
+    @availible_flights = nil
+    @airport_options = Airport.all.map { |airport| [ airport.identifier, airport.id ] }
+    @date_options = Flight.order('start_datetime ASC').all.map { |flight| [ flight.start_datetime.strftime("%m/%d/%Y") ] }
+    
+    if searched?
+      @date = Date.strptime(params[:date], "%m/%d/%Y")
+      @availible_flights = Flight.where(departure_airport: params[:departure], arrival_airport: params[:arrival], start_datetime: @date.beginning_of_day..@date.end_of_day)
+    end
+  end
+
+  private
+    def searched?
+      params[:departure] && params[:arrival] && params[:passengers] && params[:date]
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+airports = Airport.create([{identifier: 'SFO'}, {identifier: 'JFK'}, {identifier: 'DFW'}, {identifier: 'SLC'}])
+
+flights = Flight.create([{departure_airport_id: 1, arrival_airport_id: 2, start_datetime: DateTime.new(2022, 1, 11, 12, 30, 0), flight_duration: 325},
+                         {departure_airport_id: 1, arrival_airport_id: 3, start_datetime: DateTime.new(2022, 1, 2, 11, 00, 0), flight_duration: 215},
+                         {departure_airport_id: 1, arrival_airport_id: 4, start_datetime: DateTime.new(2022, 2, 4, 2, 30, 0), flight_duration: 115},
+                         {departure_airport_id: 2, arrival_airport_id: 1, start_datetime: DateTime.new(2022, 3, 1, 3, 30, 0), flight_duration: 325},
+                         {departure_airport_id: 2, arrival_airport_id: 3, start_datetime: DateTime.new(2022, 1, 21, 4, 30, 0), flight_duration: 255},
+                         {departure_airport_id: 2, arrival_airport_id: 4, start_datetime: DateTime.new(2022, 2, 8, 21, 15, 0), flight_duration: 330},
+                         {departure_airport_id: 3, arrival_airport_id: 1, start_datetime: DateTime.new(2022, 7, 8, 18, 20, 0), flight_duration: 215},
+                         {departure_airport_id: 3, arrival_airport_id: 2, start_datetime: DateTime.new(2022, 9, 21, 0, 40, 0), flight_duration: 225},
+                         {departure_airport_id: 3, arrival_airport_id: 4, start_datetime: DateTime.new(2022, 1, 11, 11, 0, 0), flight_duration: 175},
+                         {departure_airport_id: 4, arrival_airport_id: 1, start_datetime: DateTime.new(2022, 1, 1, 20, 45, 0), flight_duration: 115},
+                         {departure_airport_id: 4, arrival_airport_id: 2, start_datetime: DateTime.new(2022, 1, 2, 3, 0, 0), flight_duration: 330},
+                         {departure_airport_id: 4, arrival_airport_id: 3, start_datetime: DateTime.new(2022, 2, 3, 12, 20, 0), flight_duration: 175}]) 
