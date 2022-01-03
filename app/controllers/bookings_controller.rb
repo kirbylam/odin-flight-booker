b99fba55a76b340132248921f74ef4f4c8a5ca46
kirby_lam <kirbylam16@gmail.com>
2022-01-03 03:14:35 -0800

    Users can now put in passenger info

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,34 @@
 class BookingsController < ApplicationController
   def new
+    puts 'new'
     @booking = Booking.new
-    @flight = params[:flight]
-    @tickets = params[:tickets]
+    @flight = params[:booking].to_i
+    @tickets = params[:tickets].to_i
+    @tickets.times { @booking.passengers.build }
   end
 
   def create
     @booking = Booking.new(booking_params)
+    if @booking.save
+      puts "saved"
+    else
+      puts "error"
+      render :new
+    end
   end
 
   private
     def booking_params
-      params.require(:booking).permit(:name, :email)
+      params.require(:booking).permit(:flight_id, :seats, passengers_attributes: [:name, :email])
+    end
+
+    def passengers_valid?
+      params[:booking][:seats].to_i.times do |i|
+        @passenger = Passenger.new(name: params[:booking][:passengers_attributes]["#{i}"][:name], email: params[:booking][:passengers_attributes]["#{i}"][:email])
+        unless @passenger.valid?
+          flash[:warning] = 'You entered an invalid entry!'
+          render :new
+        end
+      end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'flights#index'
 
-  resource :bookings, only: [:new]
   resource :flights, only: [:index]
+  resources :bookings, only: [:new, :create]
 end
